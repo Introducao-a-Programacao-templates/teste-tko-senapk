22eef831f17b9c73a81c448f400b59b17bdeb62e
David Sena <sena.ufc@gmail.com>
2025-05-31 21:09:02 -0300

    fix test script

--- classroom/test.ts
+++ classroom/test.ts
@@ -48,7 +48,7 @@ function run_tko(folder: string) {
         total += 1;
         // testa primeiro se roda
         const output = execSync(
-            `tko -w 140 run -d ${folder} --eval`
+            `tko -w 140 run -d ${folder}/cases.tio ${folder}/draft.ts --eval`
         ).toString();
         let lines = output.split('\n');
         let resume = lines[1]
